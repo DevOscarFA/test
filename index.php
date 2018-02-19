b6255e71f6fd94db95ea94b33639df2be0d2d79d
Oscar Fernandez Alzate <ofernandez@viajemos.com>
2018-02-19 12:22:35 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	$mystring = 'filter_start_date_from';
+	$findme   = '_';
+	$posInitial = strpos($mystring, $findme);
+	$posFinal = strlen ( $mystring );
+
+	echo substr($mystring,$posInitial+1,$posFinal);
+
+?>
